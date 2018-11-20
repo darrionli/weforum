afbe7bf9644cef7f50d83597e69fd5c32d8d6fd0
lidi <lidi0903@163.com>
2018-11-20 21:50:18 +0800

    finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
+	// 注册页
     public function create()
     {
     	return view('users/create');
+    }
+
+    // 个人资料显示页
+    public function show(User $user)
+    {
+    	return view('users.show', compact('user'));
+    }
+
+    // 注册
+    public function store(Request $request)
+    {
+    	$this->validate($request, [
+    		'name' => 'required|max:50',
+    		'email' => 'required|email|unique:users|max:255',
+    		'password' => 'required|confirmed|min:6'
+    	]);
+
+    	$user = User::create([
+    		'name' => $request->name,
+    		'email' => $request->email,
+    		'password' => bcrypt($request->password)
+    	]);
+
+    	session()->flash('success', '欢迎，您将开启一段新的旅程~');
+    	return redirect()->route('users.show', [$user]);
     }
 }
